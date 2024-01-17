d4ebecdfe2eeef3aabab35077aa9559265a2bd8e
Tonyelchediac <130777962+Tonyelchediac@users.noreply.github.com>
2024-01-17 16:10:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,3 @@
-let words = document.querySelectorAll(".word");
-words.forEach((word)=>{
-    let letters = word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letter)=>{
-        let span = document.createElement("span");
-        span.textContent = letter;
-        span.className = "letter";
-        word.append(span);
-    })
-});
-
-let curentWordIndex = 0;
-let maxWordINdex = words.length -1;
-words[curentWordIndex].style.opacity = "1";
-
-let changeText = ()=>{
-    let curentWord = words[curentWordIndex];
-    let nextword = curentWordIndex === maxWordINdex ? words[0] : words[curentWordIndex + 1];
-
-    Array.from(curentWord.children).forEach((letter,i)=>{
-        setTimeout(()=>{
-            letter.className = "letter out";
-        },i * 80);
-    });
-    nextword.style.opacity="1";
-    Array.from(nextword.children).forEach((letter,i)=>{
-        letter.className = "letter behind";
-        setTimeout(()=>{
-            letter.className = "letter in";
-        },340 + i * 80);
-    });
-    curentWordIndex = curentWordIndex === maxWordINdex ? 0 : curentWordIndex + 1;
-};
-
-changeText();
-setInterval(changeText,3000)
-
-
 ////////sticky navlist////////////////////////////////////////////////////
 
 let menuLi = document.querySelectorAll('header ul li a');
@@ -51,12 +12,6 @@ function activeMenu(){
 
 activeMenu();
 window.addEventListener("scroll",activeMenu);
-
-
-
-
-
-
 
 const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
@@ -86,6 +41,7 @@ function openMenu() {
   document.body.classList.add('menu-open');
   isMenuOpen = true;
 }
+
 
 function closeMenu() {
   menu.classList.remove('open');
@@ -145,7 +101,11 @@ sr.reveal('.service3')
 sr.reveal('.main-text')
 
 
-
+$(document).ready(function() {
+  $('.burger').click(function() {
+    $('.navbar').toggleClass('open');
+  });
+});
 
 
 
